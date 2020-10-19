f0a49abc527bd1c0d88fd1cb487fbe0790bb6e9a
Connor Jordan <56240039+ComputerConnor@users.noreply.github.com>
2020-10-18 20:24:36 -0500

    Final CAC Commit

---
+++ Assets/Scripts/characterController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterController : MonoBehaviour
+{
+
+    public float speed = 10.0F;
+    public Rigidbody rb;
+
+    bool aiming;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float translation = Input.GetAxis("Vertical") * speed;
+        float straffe = Input.GetAxis("Horizontal") * speed;
+        translation *= Time.deltaTime;
+        straffe *= Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            aiming = true;
+        }
+
+        else if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            aiming = false;
+        }
+
+        if (!aiming)
+        {
+            transform.Translate(straffe, 0, translation);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            rb.AddForce(new Vector3(0, 3, 0), ForceMode.Impulse);
+
+        if (Input.GetKeyDown("escape"))
+            Cursor.lockState = CursorLockMode.None;
+
+    }
+}
